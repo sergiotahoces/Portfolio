c5cbaa19535f8ce1f1b50980632c74c6eb166ce4
sergiotahoces <sergio@MacBook-Pro-de-Sergio.local>
2020-02-11 11:11:20 +0100

    Cuarto Commit: Funcionalidades correctas

--- java.js
+++ java.js
@@ -1,36 +1,16 @@
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var palabra;
-palabra = txt;
-var speed = 200; /* The speed/duration of the effect in milliseconds */
-const nombre = document.querySelector("#demo");
-
-function redirect() {
-    var url = "https://www.facebook.com/sergio.tahocesguitian";
-    window.location(url);
-}
-
-function typeWriter() {
-    for (let i = 0; i < txt.length; i++) {
-        nombre.innerText += txt.charAt(i);
-        setTimeout(typeWriter, speed);
-    }
-    // Call carousel manually
-    $('#myCarouselCustom').carousel();
-
-    // Go to the previous item
-    $("#prevBtn").click(function () {
-        $("#myCarouselCustom").carousel("prev");
-    });
-    // Go to the previous item
-    $("#nextBtn").click(function () {
-        $("#myCarouselCustom").carousel("next");
-    });
-
-    //   if (i < txt.length) {
-    //     p.innerText += txt.charAt(i);
-    //     i++;
-    //     setTimeout(typeWriter, speed);
-    //   }
-}
-typeWriter();
+function ocupation(container, text, interval) {
+    size = text.length;
+    slot = document.querySelector(container);
+    let i = 0;
+    timer = setInterval(function () {
+        slot.innerHTML = slot.innerHTML.substr(0,slot.innerHTML.length-1)+text.charAt(i)+".";
+        if (i >= size) {
+            clearInterval(timer);
+            return true;
+        } else {
+            i++;
+        }
+    }, interval);
+};
+const text = 'Software Developer';
+ocupation(".ocupation", text, 100);
